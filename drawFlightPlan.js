392030c6bf137daf599ddab757c97d7724e548de
t0mday <95492549+t0mday@users.noreply.github.com>
2022-01-01 20:30:04 +0000

    improvements and refactoring

--- drawFlightPlan.js
+++ drawFlightPlan.js
@@ -6,28 +6,55 @@ const fs = require('fs');
 const { getMissionWaypoints, getFlightBoundary } = require('./readFlightData');
 const {createCanvas, loadImage} = require('canvas');
 
-// set canvas size and drawing context
-const width = 1000;
-const height = 1000;
-const canvas = createCanvas(width, height);
-const ctx = canvas.getContext('2d');
-
 // Import the mission and boundary shapes, converting lat, long into x, y (reversed order)
 const waypoints = getMissionWaypoints('drone-mission-example.json', false);
 const boundary = getFlightBoundary('flight-boundary-example.json', false);
 
-// Transform mission and boundary shapes to canvas extent
+// Find min and max coords of data
 const minMaxCoords = findMinMax([boundary, waypoints]);
+
+// set canvas size, ensuring correct aspect ratio to fit the data
+const width = 1000;
+const height = getCanvasHeight();
+
+// returns canvas height in relative units
+function getCanvasHeight() {
+    // calculate lat, long degrees between min and max coords
+    const longDeg = minMaxCoords.xMax - minMaxCoords.xMin;
+    const latDeg = minMaxCoords.yMax - minMaxCoords.yMin;
+
+    // convert lat, long degrees to km
+    const latKm = 110.574 * latDeg;
+    const longKm = 111.32 * Math.cos(deg2rad(minMaxCoords.yMin)) * longDeg;
+
+    // helper conversion for degrees to radians
+    function deg2rad(d) {
+        return d/180*Math.PI; 
+    }
+
+    return width / longKm * latKm;
+}
+
+
+// scale data to canvas size
 const scaledWaypoints = transformCoords(waypoints, width, height, minMaxCoords);
 const scaledBoundary = transformCoords(boundary, width, height, minMaxCoords);
+
+// create canvas and set drawing context
+const canvas = createCanvas(width, height);
+const ctx = canvas.getContext('2d');
 
 // Import background image and draw the mission and boundary polygons on top
 loadImage('./basemap.png').then(image => {
     // draw background image to fill canvas
-    ctx.drawImage(image, 0, 0, 1000, 1000);
+    ctx.drawImage(image, 0, 0, width, height);
     // draw waypoints and boundary
-    draw(scaledWaypoints, 'blue');
-    draw(scaledBoundary, 'orange');
+    draw(scaledWaypoints, 'purple', 7);
+    draw(scaledBoundary, 'orange', 5);
+    
+    // acknowledge map data in background image
+    writeAcknowledgements('Map Imagery and Data \u00A9 2022 Maxar Technologies');
+
     // save to file
     savePng('flight.png');
 });
@@ -66,15 +93,23 @@ function transformCoords(coords, width, height, {xMin, yMin, xMax, yMax}) {
     })); 
 }
 
-// Draws shape to canvas in chosen colour.
-function draw(shape, colour) {
+// Draws shape to canvas in chosen colour and line width
+function draw(shape, colour, lineWidth) {
     ctx.strokeStyle = colour;
+    ctx.lineWidth = lineWidth;
     ctx.beginPath();
     ctx.moveTo(shape[0].x, shape[0].y);
     for(let i=1; i<shape.length; i++) {
         ctx.lineTo(shape[i].x, shape[i].y);
     }
     ctx.stroke();
+}
+
+// Writes map data acknowledgments
+function writeAcknowledgements(text) {
+    ctx.font = 'bold 16pt Helvetica';
+    ctx.fillStyle = '#fff';
+    ctx.fillText(text, 50, height - 30);
 }
 
 // Creates png image file from canvas
